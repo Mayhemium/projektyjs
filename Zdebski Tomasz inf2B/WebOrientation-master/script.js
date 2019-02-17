3689f686fba7d876df128803341223d73de7bf3d
mayhemium <tomzdebski@gmail.com>
2019-02-18 00:38:49 +0100

    working

--- Zdebski Tomasz inf2B/WebOrientation-master/script.js
+++ Zdebski Tomasz inf2B/WebOrientation-master/script.js
@@ -1,6 +1,8 @@
 let ballLeft = 340;
     let ballTop = 340;
     let ball = document.getElementsByClassName("ball")[0];
+    let holes = document.getElementsByClassName("hole");
+    let winHole = document.getElementsByClassName("win")[0];
     let x = 0;
     let z = 0;
     window.addEventListener('deviceorientation', (e)=> {
@@ -19,7 +21,50 @@ let ballLeft = 340;
             gameOver();
         }
         ball.style.top = ballTop + "px";
+
+        check(ballLeft+10,ballTop+10);
     }
+
+    function check(x,y){
+        for (let i=0; i<holes.length; i++) {
+            let top = parseInt(holes[i].style.top.split("px")[0])+10;
+            let left = parseInt(holes[i].style.left.split("px")[0])+10;
+
+            var a = x - left;
+            var b = y - top;
+
+            var c = Math.sqrt( a*a + b*b );
+
+            if(c<=10)
+                gameOver();
+        }
+
+
+        let top = parseInt(winHole.style.top.split("px")[0])+10;
+        let left = parseInt(winHole.style.left.split("px")[0])+10;
+
+        var a = x - left;
+        var b = y - top;
+
+        var c = Math.sqrt( a*a + b*b );
+
+        if(c<=10)
+            won();
+    }
+
+    function won(){
+        clearInterval(myInterval);
+
+        ballLeft = 340;
+        ballTop = 340;
+        ball.style.left = ballLeft + "px";
+        ball.style.top = ballTop + "px";
+
+        if(confirm("You won\nPlay again?")){
+            myInterval = setInterval(ballMove,16);
+        }
+    }
+
     function gameOver(){
         clearInterval(myInterval);
 
