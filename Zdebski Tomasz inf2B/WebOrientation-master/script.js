591be367d7d84e72d43710f09fbc3c8dd6e3e420
mayhemium <tomzdebski@gmail.com>
2019-03-14 04:41:42 +0100

    time + comment

--- Zdebski Tomasz inf2B/WebOrientation-master/script.js
+++ Zdebski Tomasz inf2B/WebOrientation-master/script.js
@@ -1,47 +1,45 @@
 let ballLeft = 340;
-    let ballTop = 340;
-    let ball = document.getElementsByClassName("ball")[0];
-    let holes = document.getElementsByClassName("hole");
-    let winHole = document.getElementsByClassName("win")[0];
-    let x = 0;
-    let z = 0;
-    window.addEventListener('deviceorientation', (e)=> {
-        x = event.beta;  // In degree in the range [-180,180]
-        z = event.gamma; // In degree in the range [-90,90]
-    });
-    let myInterval = setInterval(ballMove,16);
-    function ballMove(e){
-        ballLeft = ballLeft + z/4;
-        if(ballLeft<=0||ballLeft>=660){
-            gameOver();
-        }
-        ball.style.left = ballLeft + "px";
-        ballTop = ballTop + x/4;
-        if(ballTop<=0||ballTop>=660){
-            gameOver();
-        }
-        ball.style.top = ballTop + "px";
+let ballTop = 340;
+let ball = document.getElementsByClassName("ball")[0];
+let holes = document.getElementsByClassName("hole");
+let winHole = document.getElementsByClassName("win")[0];
+let x = 0;
+let z = 0;
+let startTime = Date.now();
 
-        check(ballLeft+10,ballTop+10);
+//sciagniecie orientacji
+window.addEventListener('deviceorientation', (event)=> {
+    x = event.beta;  // In degree in the range [-180,180]
+    z = event.gamma; // In degree in the range [-90,90]
+});
+
+//ruszanie pilka
+let myInterval = setInterval(ballMove,16);
+function ballMove(e){
+    ballLeft = ballLeft + z/4;
+
+    //wyjechanie poza obszar horyzontalnie
+    if(ballLeft<=0||ballLeft>=660){
+        gameOver();
     }
+    ball.style.left = ballLeft + "px";
+    ballTop = ballTop + x/4;
 
-    function check(x,y){
-        for (let i=0; i<holes.length; i++) {
-            let top = parseInt(holes[i].style.top.split("px")[0])+10;
-            let left = parseInt(holes[i].style.left.split("px")[0])+10;
+    //wyjechanie poza obszar wertykalnie
+    if(ballTop<=0||ballTop>=660){
+        gameOver();
+    }
+    ball.style.top = ballTop + "px";
 
-            var a = x - left;
-            var b = y - top;
+    //sprawdzenie kolizji
+    check(ballLeft+10,ballTop+10);
+}
 
-            var c = Math.sqrt( a*a + b*b );
-
-            if(c<=10)
-                gameOver();
-        }
-
-
-        let top = parseInt(winHole.style.top.split("px")[0])+10;
-        let left = parseInt(winHole.style.left.split("px")[0])+10;
+//obliczenie kolizji z pitagorasa
+function check(x,y){
+    for (let i=0; i<holes.length; i++) {
+        let top = parseInt(holes[i].style.top.split("px")[0])+10;
+        let left = parseInt(holes[i].style.left.split("px")[0])+10;
 
         var a = x - left;
         var b = y - top;
@@ -49,31 +47,51 @@ let ballLeft = 340;
         var c = Math.sqrt( a*a + b*b );
 
         if(c<=10)
-            won();
+            gameOver();
     }
 
-    function won(){
-        clearInterval(myInterval);
 
-        ballLeft = 340;
-        ballTop = 340;
-        ball.style.left = ballLeft + "px";
-        ball.style.top = ballTop + "px";
+    let top = parseInt(winHole.style.top.split("px")[0])+10;
+    let left = parseInt(winHole.style.left.split("px")[0])+10;
 
-        if(confirm("You won\nPlay again?")){
-            myInterval = setInterval(ballMove,16);
-        }
+    var a = x - left;
+    var b = y - top;
+
+    var c = Math.sqrt( a*a + b*b );
+
+    if(c<=10)
+        won();
+}
+
+
+//zresetowanie i wyswietlenie wygranej
+function won(){
+    clearInterval(myInterval);
+
+    ballLeft = 340;
+    ballTop = 340;
+    ball.style.left = ballLeft + "px";
+    ball.style.top = ballTop + "px";
+    let time = new Date(Date.now()-startTime);
+
+    if(confirm("You won\nTime: "+time.toLocaleTimeString().slice(3,8)+"\nPlay again?")){
+        myInterval = setInterval(ballMove,16);
+        startTime = Date.now();
     }
+}
 
-    function gameOver(){
-        clearInterval(myInterval);
+//zresetowanie i wyswietlenie przegranej
+function gameOver(){
+    clearInterval(myInterval);
 
-        ballLeft = 340;
-        ballTop = 340;
-        ball.style.left = ballLeft + "px";
-        ball.style.top = ballTop + "px";
+    ballLeft = 340;
+    ballTop = 340;
+    ball.style.left = ballLeft + "px";
+    ball.style.top = ballTop + "px";
+    let time = new Date(Date.now()-startTime);
 
-        if(confirm("Game Over\nPlay again?")){
-            myInterval = setInterval(ballMove,16);
-        }
+    if(confirm("Game Over\nTime: "+time.toLocaleTimeString().slice(3,8)+"\nPlay again?")){
+        myInterval = setInterval(ballMove,16);
+        startTime = Date.now();
     }
+}
